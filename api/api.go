5960ea424930ae16da82f220fcbc0595eeda25a1
Adrian Punga <adrian.punga@ejobsgroup.ro>
2020-12-21 09:08:18 +0200

    Version bump

--- api/api.go
+++ api/api.go
@@ -79,7 +79,7 @@ func Init(apiMux *mux.Router) error {
 		rw.WriteHeader(200)
 		jsonEnc := json.NewEncoder(rw)
 		err := jsonEnc.Encode(version{
-			Version: "v0.2.1",
+			Version: "v0.3.0",
 		})
 		if err != nil {
 			rw.WriteHeader(500)
